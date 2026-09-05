eea6630b56eca55f75299eb3430a1efabb96d7f9
SAE Admin <admin@sae.smakpal.sch.id>
2026-09-05 22:33:25 +0700

    chore: keep only 3 images logo-dark, logo-light, logo-icon

--- public/js/sae.js
+++ public/js/sae.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Theme Switcher (Hanya elemen ID themeToggleBtn)
     const themeToggleBtns = document.querySelectorAll("#themeToggleBtn");
-    const logos = document.querySelectorAll("#navLogo, #dashLogo, #loginLogo, #installLogo");
+    const logos = document.querySelectorAll(
+        "#navLogo, #dashLogo, #loginLogo, #installLogo",
+    );
     const htmlElement = document.documentElement;
 
     const applyTheme = (theme) => {
